a486c5dc2b796ff0b0dbecf0894a267d79c4be51
Ciaran Cain <ccain@dev.terastrm.net>
2024-09-25 18:27:01 +0000

    Dummy hpack module

---
+++ src/hpack.ext.c
@@ -0,0 +1,11 @@
+void hpackQ___ext_init__() {
+}
+
+B_bytes hpackQ_deflate(B_dict headers) {
+    return to$bytes("");
+}
+
+B_dict hpackQ_inflate(B_bytes compressed_headers) {
+    B_dict ret;
+    return ret;
+}
