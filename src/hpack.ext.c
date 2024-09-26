efd4f024defa76f6bb3404e0a0a6e2086c5e1100
Ciaran Cain <ccain@dev.terastrm.net>
2024-09-26 20:33:15 +0000

    Restructure hpack module to being class-based so compression context can persist during session

--- src/hpack.ext.c
+++ src/hpack.ext.c
@@ -5,17 +5,46 @@
 void hpackQ___ext_init__() {
 }
 
-B_bytes hpackQ_deflate(B_dict headers) {
+struct hpackQ_DeflaterG_class;
+
+struct hpackQ_Deflater {
+    struct hpackQ_DeflaterG_class *$class;
     nghttp2_hd_deflater *deflater;
-    nghttp2_hd_deflate_new(&deflater, DEFAULT_MAX_BUFLEN);
-    nghttp2_hd_deflate_del(deflater);
+};
+
+B_NoneType hpackQ_DeflaterD___init__(hpackQ_Deflater self) {
+    nghttp2_hd_deflate_new(&self->deflater, DEFAULT_MAX_BUFLEN);
+    return B_None;
+}
+
+B_NoneType hpackQ_DeflaterD___del__(hpackQ_Deflater self) {
+    nghttp2_hd_deflate_del(self->deflater);
+    return B_None;
+}
+
+B_bytes hpackQ_DeflaterD_deflate(hpackQ_Deflater self, B_dict headers) {
     return to$bytes("");
+}
+
+
+struct hpackQ_InflaterG_class;
+
+struct hpackQ_Inflater {
+    struct hpackQ_InflaterG_class *$class;
+    nghttp2_hd_inflater *inflater;
+};
+
+B_NoneType hpackQ_InflaterD___init__(hpackQ_Inflater self) {
+    nghttp2_hd_inflate_new(&self->inflater);
+    return B_None;
+}
+
+B_NoneType hpackQ_InflaterD___del__(hpackQ_Inflater self) {
+    nghttp2_hd_inflate_del(self->inflater);
+    return B_None;
 }
 
 B_dict hpackQ_inflate(B_bytes compressed_headers) {
     B_dict ret;
-    nghttp2_hd_inflater *inflater;
-    nghttp2_hd_inflate_new(&inflater);
-    nghttp2_hd_inflate_del(inflater);
     return ret;
 }
