eb8e3423ba06e2f78e027e06476e960919cbe33d
Ciaran Cain <ccain@dev.terastrm.net>
2024-09-30 20:40:27 +0000

    Inflate method

--- src/hpack.ext.c
+++ src/hpack.ext.c
@@ -44,7 +44,50 @@ B_NoneType hpackQ_InflaterD___del__(hpackQ_Inflater self) {
     return B_None;
 }
 
-B_dict hpackQ_inflate(B_bytes compressed_headers) {
+B_dict hpackQ_InflaterD_inflate(hpackQ_Inflater self, B_bytes compressed_headers) {
     B_dict ret;
+
+    size_t inlen = (size_t)compressed_headers->nbytes;
+    uint8_t *in = (uint8_t*)compressed_headers->str;
+    
+    nghttp2_ssize inflate_ret;
+    nghttp2_nv nv_out;
+    int inflate_flags;
+
+    B_Hashable wit = (B_Hashable)B_HashableD_strG_witness;
+
+    while (1) {
+        inflate_ret = nghttp2_hd_inflate_hd3(self->inflater, &nv_out, &inflate_flags, in, inlen, 1);
+
+	if (inflate_ret < 0) {
+            // Need to handle these error cases more gracefully. Maybe an optional?
+            break;
+	}
+
+	in += inflate_ret;
+	inlen -= inflate_ret;
+
+	if (inflate_flags & NGHTTP2_HD_INFLATE_EMIT) {
+            char *name = acton_malloc(nv_out.namelen + 1);
+	    char *value = acton_malloc(nv_out.valuelen + 1);
+
+	    strncpy(name, (char*)nv_out.name, nv_out.namelen);
+	    strncpy(value, (char*)nv_out.value, nv_out.valuelen);
+
+	    B_dictD_setitem(ret, wit, name, value);
+	}
+
+	if (inflate_flags & NGHTTP2_HD_INFLATE_FINAL) {
+            nghttp2_hd_inflate_end_headers(self->inflater);
+            break;
+	}
+
+	if ((inflate_flags & NGHTTP2_HD_INFLATE_EMIT) == 0 &&
+            inlen == 0) {
+            break;
+	}
+
+    }
+
     return ret;
 }
